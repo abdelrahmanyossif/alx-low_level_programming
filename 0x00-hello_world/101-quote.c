8ec53806aa77369764efb8811e7f63e30e5f7e47
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-23 07:50:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main -Entry point
  *
  * Description: print a qoute usining write function
- *      write(int fd.const void *buf.sixe_t count)
+ *      ssize_t write(int fd, const void *buf, sixe_t count)
  *
  * Return: 1 (not success)
  */
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n;
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1,qou,59);
 	return (1);
