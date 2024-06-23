cf4e4f53ad771a465beab507e2cda5879681c8c6
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-23 06:58:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long num2;
 	float f;
 
-	printf("size of a char: %u byte(s)\n", sizeof(c));
-	printf("size of an int: %u byte(s)\n", sizeof(num));
-	printf("size of a long int: %u byte(s)\n", sizeof(num1));
-	printf("size of a long long int: %u byte(s)\n", sizeof(num2));
-	printf("size of a float: %u byte(s)\n", sizeof(f));
+	printf("size of a char: %lu byte(s)\n", sizeof(c));
+	printf("size of an int: %lu byte(s)\n", sizeof(num));
+	printf("size of a long int: %lu byte(s)\n", sizeof(num1));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(num2));
+	printf("size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
