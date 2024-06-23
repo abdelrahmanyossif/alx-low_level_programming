6f8a0ce6bc612417e0386f644a745a00e36ff7ab
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-23 06:14:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,15 @@
 
 /**
  * Main - print the string with the puts function
+ *
  * description: print "Programming is like building a multilingual puzzle"
- * Function: main
- * Entry point for the program
+ * Main - Entry point of the program
+ * 
  * Return: Always 0.
  */
 
-int main(void)/* Entry point for the program
-*/
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
