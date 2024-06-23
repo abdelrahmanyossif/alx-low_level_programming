76761ebd526cb79424137d8c34cd9cdd6449011c
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-23 06:18:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,12 @@
  *
  * description: print "Programming is like building a multilingual puzzle"
  * Main - Entry point of the program
- * 
+ *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
