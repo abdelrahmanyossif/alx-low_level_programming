04f73a3284e702a968fcd3f098e73cb56bcc1ce4
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-23 07:47:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main -Entry point 
+ * main -Entry point
  *
  * Description: print a qoute usining write function
  *      write(int fd.const void *buf.sixe_t count)
@@ -13,6 +13,7 @@
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n;
+
 	write(1,qou,59);
 	return (1);
 }
