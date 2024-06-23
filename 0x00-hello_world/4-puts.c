32f40d8f4c538b38346a0b1e1054972e0cc636dc
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-23 06:04:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,8 @@
 /**
  * Main - print the string with the puts function
  * description: print "Programming is like building a multilingual puzzle"
+ * Function: main
+ * Entry point for the program
  * Return: Always 0.
  */
 
