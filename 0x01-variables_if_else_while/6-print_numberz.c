dc8fadb371b6571ccc2c33eeaca9349787f7c76a
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-24 10:52:26 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
