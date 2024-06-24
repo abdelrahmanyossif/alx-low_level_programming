0d69bd9662bf3a6affd3ad6f276f9c60e8718095
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-24 08:59:24 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,18 +6,19 @@
  *
  * Description: print the alphabet in lowercase
  *
- * Return: (0) (success)
+ * Return: Always (0) (success)
 */
 
 int main(void)
 {
 	char ch = 'a';
-	while ('a' <= 'z')
+
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
 
-	return 0;
+	return (0);
 }
