b38e008ab7a1d0f5772325b081cd5f1f697b79e3
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2024-06-28 16:24:01 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Do not print 2 and 4
+ *
+ * Return: 1 if its uppercase, 0 if not
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	for (n = 0; n >= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+
+}
