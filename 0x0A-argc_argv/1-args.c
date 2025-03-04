dfed462b01f5ea911b29724a5ed13828a1629947
abdelrahmanyossif <abdelrahmanyossif949@gmail.com>
2025-03-04 10:51:58 -0800

    gv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints the name number of arguments passed to it
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: arguments
  *
- * Return: always 0
+ * Return: 0
  */
-
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char **argv)
 {
+	(void)argv;
+
 	printf("%d\n", argc - 1);
 	return (0);
 }
